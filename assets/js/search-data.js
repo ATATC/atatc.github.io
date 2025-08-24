4455ecd4ede58aaa8313489e0f52b12680a51f01
ATATC <futerry@outlook.com>
2025-08-24 21:23:30 +0000

    Deploying to gh-pages from @ ATATC/atatc.github.io@f9e925a1b900c88c870903ce3f0b8b6f2df1224f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,12 +51,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
+        },{id: "nav-lab-members",
+          title: "lab members",
+          description: "realize your innovational ideas with TFu Lab",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/people/";
+            window.location.href = "/lab/";
           },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
