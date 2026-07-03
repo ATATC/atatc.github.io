98c0c353a6866062a970fd0d5312861d26916b59
ATATC <futerry@outlook.com>
2026-07-03 11:32:08 -0400

    Docs.

--- purgecss.config.js
+++ purgecss.config.js
@@ -2,5 +2,6 @@ module.exports = {
   content: ["_site/**/*.html", "_site/**/*.js"],
   css: ["_site/assets/css/*.css"],
   output: "_site/assets/css/",
+  safelist: ["pagination", "pagination-lg", "justify-content-center", "page-item", "page-link", "active", "disabled"],
   skippedContentGlobs: ["_site/assets/**/*.html"],
 };
